5994e13ce3e5e74365eaafd039c3b4338c1e055a
Tanvir <engr.cmtanvir@gmail.com>
2021-04-19 01:05:55 +0600

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -23,6 +25,50 @@ class MyMusic extends StatefulWidget {
 class _MyMusicState extends State<MyMusic> {
   bool playing = false;
   IconData playBtn = Icons.play_arrow;
+  AudioPlayer _player;
+  AudioCache cache;
+  Duration position = new Duration();
+  Duration musicLength = new Duration();
+  Widget slider(){
+    return Container(
+      width: 300.0,
+      child: Slider.adaptive(
+        activeColor: Colors.blue[800],
+        inactiveColor: Colors.greenAccent[350],
+        value: position.inSeconds.toDouble(),
+        max: musicLength.inSeconds.toDouble(),
+        onChanged: (value){
+          seekToSec(value.toInt());
+        }),
+    );
+  }
+
+  void seekToSec(int sec){
+    Duration newPos = Duration(seconds: sec);
+    _player.seek(newPos);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _player = AudioPlayer();
+    cache = AudioCache(fixedPlayer: _player);
+
+    _player.durationHandler = (d) {
+      setState(() {
+        musicLength = d;
+      });
+    };
+
+    _player.positionHandler = (p) {
+      setState(() {
+        position = p;
+      });
+    };
+
+    cache.load("Ekla holei bujhte pari.mp3");
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -107,38 +153,58 @@ class _MyMusicState extends State<MyMusic> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
+                        Container(
+                          width: 500.0,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text("${position.inMinutes}:${position.inSeconds
+                                  .remainder(60)}",style: TextStyle(fontSize:
+                              18.0),),
+                              slider(),
+                              Text("${musicLength.inMinutes}:${musicLength.inSeconds
+                                  .remainder(60)}",style: TextStyle(fontSize:
+                                  18.0),),
+                            ],
+                          ),
+                        ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
                             IconButton(
                                 iconSize: 45.0,
-                                color: Colors.green,
-                                icon: Icon(Icons.skip_previous),
+                                color: Colors.blue,
                                 onPressed: (){},
-                                ),
+                                icon: Icon(Icons.skip_previous),
+                               ),
                             IconButton(
                               iconSize: 62.0,
-                              color: Colors.green[800],
-                              icon: Icon(Icons.play_arrow),
-                              onPressed: (){
+                              color: Colors.blue[800],
+                              onPressed: () {
                                 if(!playing){
+                                  cache.play("Ekla.mp3");
                                   setState(() {
+                                    _player.pause();
                                     playBtn = Icons.pause;
                                     playing=true;
                                   });
                                 }else{
-                                  playBtn = Icons.play_arrow;
-                                  playing=false;
+                                  setState(() {
+                                    playBtn = Icons.play_arrow;
+                                    playing=false;
+                                  });
                                 }
                               },
-                            ),
+                              icon: Icon(playBtn),
+                              ),
                             IconButton(
                               iconSize: 45.0,
-                              color: Colors.green,
-                              icon: Icon(Icons.skip_next),
+                              color: Colors.blue,
                               onPressed: (){},
-                            ),
+                              icon: Icon(Icons.skip_next),
+                              ),
                           ],
                         ),
                       ],
